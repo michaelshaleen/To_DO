eff43fef35df495c39c25fdf71dac37416946de7
michaelshaleen <shaleenmichael@yahoo.com>
2022-10-04 21:11:39 -0500

    add button appends to DOM, had a bug fixed now, issue was   const toDoDiv = document.createElement(div), div needed qdouble quotes instead of singles

--- To_Do_List/app.js
+++ To_Do_List/app.js
@@ -12,8 +12,35 @@ todoButton.addEventListener("click", addToDo)
 //Functions
 function addToDo(e){
   e.preventDefault()
-  console.log("here")
   //todo div
-  const toDoDiv = document.createElement('div');
+  const toDoDiv = document.createElement("div");
   toDoDiv.classList.add("todo");
+  console.log("div added")
+
+
+
+  const newToDo = document.createElement('li');
+  newToDo.innerText = 'hey';
+  newToDo.classList.add('todo-item')
+  toDoDiv.appendChild(newToDo);
+
+
+
+
+  // completed button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"> </i>'
+  completedButton.classList.add("complete-btn");
+  toDoDiv.appendChild(completedButton)
+  //trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+  trashButton.classList.add("complete-btn");
+  toDoDiv.appendChild(trashButton)
+
+
+  //append to list
+  todoList.appendChild(toDoDiv)
 }
+
+
