2a1b97331bfef9eff9aa8e7ab5145c967ffef0bd
michaelshaleen <shaleenmichael@yahoo.com>
2022-10-05 14:08:19 -0500

    option display completed, all, and uncompleted tasks

--- To_Do_List/app.js
+++ To_Do_List/app.js
@@ -3,13 +3,13 @@
 let todoInput = document.querySelector(".todo-input");
 let todoButton = document.querySelector(".todo-button");
 let todoList = document.querySelector(".todo-list");
-
+let filterOption = document.querySelector(".filter-todo")
 
 //Event Listeners
 // console.log(todoButton, "button")
 todoButton.addEventListener("click", addToDo);
 todoList.addEventListener("click", deleteCheck);
-
+filterOption.addEventListener("click", filterToDo);
 //Functions
 function addToDo(e){
   e.preventDefault()
@@ -64,4 +64,30 @@ function deleteCheck(e){
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterToDo(e){
+  const todos = todoList.childNodes;
+  todos.forEach(function(todo){
+    switch(e.target.value){
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if(todo.classList.contains("completed")) {
+          todo.style.display = 'flex';
+        }else{
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if(!todo.classList.contains("completed")){
+          todo.style.display = 'flex';
+        }else{
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+
 }
